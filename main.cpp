5e63fe45d7ab76d5a2ed8effad7f5a40a65db922
Ulises Ramírez Calnacasco <liracestovv.piamer16@gmail.com>
2019-02-28 21:27:38 -0600

    Iteraciones en los 4 sistemas, preguntasi el vector inical es el correcto y permite cambiarlo, al acabar un sistema pregunta si desea resolver otro, parece que la interfas esta completa pero aun hay errores desde inversa

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
 #include "libraries.h"
 #include "jacobiana.cpp"
-/*void normaEspectral(Vector &nuevo, Vector &anterior, float x){
-	Vector rno=Vector(2);
-	nuevo.restByVector(anterior, &rno);
-	to=max(abs(rno.vector[0]),abs(rno.vector[1]);
-	
-	
-	
-}*/
 
 int main(){
-	int ecu,ite;
-	int it=0;
-	int to=10;
-	int k=0;
+	int ecu,ite, p, c,t, r;
+	
 	float tol,x,y,z;
 	Vector x2=Vector(2);
 	Vector x3=Vector(3);
@@ -28,6 +18,12 @@ int main(){
 	
 	Matrix j2=Matrix(2);
 	Matrix j3=Matrix(3);
+	t=1;
+	while(t==1){
+	int it=0;
+	int to=10;
+	int k=0;
+	system("cls");
 	cout<<"Seleccione el sistema de ecuaciones que desea resolver: (digite el numero que le corresponda)"<<endl<<endl;
 
 	cout<<"1. f_1(x,y)=x^2+xy-10=0 ; f_2(x,y)= y+3xy^2-50=0 "<<endl<<endl;
@@ -57,7 +53,34 @@ int main(){
 			cin>>y;
 			x2.vector[0]=x;
 			x2.vector[1]=y;
+			c=1;
+			while(c==1){
 			
+			cout<<"¿Es correcto tu punto inicial?"<<endl<<"1. Si     2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					c=0;
+					break;
+				}
+				case 2:{
+					cout<<"Vuelve a escribrir tu punto inical(x,y)"<<endl;
+					cout<<"x=";
+					cin>>x;
+					cout<<"y=";
+					cin>>y;
+					x2.vector[0]=x;
+					x2.vector[1]=y;
+					
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
+			}
+			
+		}
 			system("cls");
 			cout<<"Resolveremos el sistema de ecuaciones:"<<endl<<endl<<"f_1(x,y)=x^2+xy-10=0 ; f_2(x,y)= y+3xy^2-50=0"
 			<<endl<<endl<<"Con una tolerancia de: "<<tol<<",  "<<ite<<" Iteraciones como maximo, "<<"Empezando en el punto inicial ("<<x<<","<<y<<")"<<endl;
@@ -80,14 +103,31 @@ int main(){
 			x2.print_vector();
 			cout<<"nuevo punto"<<endl;
 			xn2.print_vector();//de prueba
-			//to=Vector::normaEspectral(xn2,x2);
-		//	cout<<"norma: "<<to<<endl;
+			to=Vector::normaEspectral(xn2,x2);
+			cout<<"norma: "<<to<<endl;
 			x2.vector[0]=xn2.vector[0];
 			x2.vector[1]=xn2.vector[1];
 			x=x2.vector[0];
 			y=x2.vector[1];
 			k++;
 			it++;
+			}
+			cout<<"La raiz del sistema esta en:"<<endl;
+			x2.print_vector();
+			cout<<"¿Desea resolver otro sistema?"<<endl<<"1. Si      2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					break;
+				}
+				case 2:{
+					t=0;
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
 			}
 			
 			break;
@@ -101,6 +141,34 @@ int main(){
 			cin>>y;
 			x2.vector[0]=x;
 			x2.vector[1]=y;
+			c=1;
+			while(c==1){
+			
+			cout<<"¿Es correcto tu punto inicial?"<<endl<<"1. Si     2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					c=0;
+					break;
+				}
+				case 2:{
+					cout<<"Vuelve a escribrir tu punto inical(x,y)"<<endl;
+					cout<<"x=";
+					cin>>x;
+					cout<<"y=";
+					cin>>y;
+					x2.vector[0]=x;
+					x2.vector[1]=y;
+					
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
+			}
+			
+		}
 			system("cls");
 			cout<<"Resolveremos el sistema de ecuaciones:"<<endl<<endl<<"f_1(x,y)=x^2+y^2-9=0 ; f_2(x,y)=-e^x-2y-3=0"
 			<<endl<<endl<<"Con una tolerancia de: "<<tol<<",  "<<ite<<" Iteraciones como maximo, "<<"Empezando en el punto inicial ("<<x<<","<<y<<")"<<endl;
@@ -123,14 +191,31 @@ int main(){
 			x2.print_vector();
 			cout<<"nuevo punto"<<endl;
 			xn2.print_vector();//de prueba
-			//to=Vector::normaEspectral(xn2,x2);
-		//	cout<<"norma: "<<to<<endl;
+			to=Vector::normaEspectral(xn2,x2);
+			cout<<"norma: "<<to<<endl;
 			x2.vector[0]=xn2.vector[0];
 			x2.vector[1]=xn2.vector[1];
 			x=x2.vector[0];
 			y=x2.vector[1];
 			k++;
 			it++;
+			}
+			cout<<"La raiz del sistema esta en:"<<endl;
+			x2.print_vector();
+			cout<<"¿Desea resolver otro sistema?"<<endl<<"1. Si      2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					break;
+				}
+				case 2:{
+					t=0;
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
 			}
 			break;
 		}
@@ -145,6 +230,37 @@ int main(){
 			x3.vector[0]=x;
 			x3.vector[1]=y;
 			x3.vector[2]=z;
+			c=1;
+			while(c==1){
+			
+			cout<<"¿Es correcto tu punto inicial?"<<endl<<"1. Si     2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					c=0;
+					break;
+				}
+				case 2:{
+					cout<<"Vuelve a escribrir tu punto inical(x,y,z)"<<endl;
+					cout<<"x=";
+					cin>>x;
+					cout<<"y=";
+					cin>>y;
+					cout<<"z=";
+					cin>>z;
+					x3.vector[0]=x;
+					x3.vector[1]=y;
+					x3.vector[2]=z;
+					
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
+			}
+			
+		}
 			system("cls");
 			cout<<"Resolveremos el sistema de ecuaciones:"<<endl<<endl<<"f_1(x,y,z)=2x^2-4x+y^2+3z^2+6z+2=0 ; f_2(x,y,z)=x^2+y^2-2y+2z^2-5=0 ; f_3(x,y,z)=3x^2-12x+y^2-3z^2+8=0"
 			<<endl<<endl<<"Con una tolerancia de: "<<tol<<",  "<<ite<<" Iteraciones como maximo, "<<"Empezando en el punto inicial ("<<x<<","<<y<<","<<z<<")"<<endl;
@@ -166,7 +282,7 @@ int main(){
 			x3.print_vector();
 			cout<<"nuevo punto"<<endl;
 			xn3.print_vector();//de prueba
-			//to=Vector::normaEspectral(xn2,x2);
+			to=Vector::normaEspectral(xn3,x3);
 			cout<<"norma: "<<to<<endl;
 			x3.vector[0]=xn3.vector[0];
 			x3.vector[1]=xn3.vector[1];
@@ -176,6 +292,23 @@ int main(){
 			z=x3.vector[2];
 			k++;
 			it++;
+			}
+			cout<<"La raiz del sistema esta en:"<<endl;
+			x3.print_vector();
+			cout<<"¿Desea resolver otro sistema?"<<endl<<"1. Si      2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					break;
+				}
+				case 2:{
+					t=0;
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
 			}
 			
 			break;
@@ -192,10 +325,86 @@ int main(){
 			x3.vector[0]=x;
 			x3.vector[1]=y;
 			x3.vector[2]=z;
+			c=1;
+			while(c==1){
+			
+			cout<<"¿Es correcto tu punto inicial?"<<endl<<"1. Si     2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					c=0;
+					break;
+				}
+				case 2:{
+					cout<<"Vuelve a escribrir tu punto inical(x,y,z)"<<endl;
+					cout<<"x=";
+					cin>>x;
+					cout<<"y=";
+					cin>>y;
+					cout<<"z=";
+					cin>>z;
+					x3.vector[0]=x;
+					x3.vector[1]=y;
+					x3.vector[2]=z;
+					
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
+			}
+			
+		}
 			system("cls");
 			cout<<"Resolveremos el sistema de ecuaciones:"<<endl<<endl<<"f_1(x,y,z)=x^2-4x+y^2=0 ; f_2(x,y,z)=x^2-x-12y+1=0 ; f_3(x,y,z)=3x^2-12x+y^2-3z^2+8=0"
 			<<endl<<endl<<"Con una tolerancia de: "<<tol<<",  "<<ite<<" Iteraciones como maximo, "<<"Empezando en el punto inicial ("<<x<<","<<y<<","<<z<<")"<<endl;
-			
+				while (it<ite/*&& to>tol*/){
+			cout<<"Iteracion #"<<k<<endl;
+			sistEcu3(x,y,z, s3); //evaluo la funcion
+			jacobiana3(x,y,z, j3);//mando a hacer la jacobiana
+			j3.print_matrix();
+			j3.get_determinant();
+			cout<<"determinante"<<endl<<j3.get_determinant()<<endl;
+			j3i=j3.invertir_matriz();//invierto la jacobiana
+			cout<<"inversa"<<endl;
+			j3i.print_matrix();//es print de prueba pero no me imprime numeros, desde aqui hay problemas
+			j3i.multiplyByVector(s3,&res3);//multiplivco inversa por evaluada
+			cout<<"resta de matriz con vector"<<endl;
+			res3.print_vector();
+			x3.restByVector(res3,&xn3);//resto punto inicial menos la multiplicacion anterior
+			cout<<"punto anterior"<<endl;
+			x3.print_vector();
+			cout<<"nuevo punto"<<endl;
+			xn3.print_vector();//de prueba
+			to=Vector::normaEspectral(xn3,x3);
+			cout<<"norma: "<<to<<endl;
+			x3.vector[0]=xn3.vector[0];
+			x3.vector[1]=xn3.vector[1];
+			x3.vector[2]=xn3.vector[2];
+			x=x3.vector[0];
+			y=x3.vector[1];
+			z=x3.vector[2];
+			k++;
+			it++;
+			}
+			cout<<"La raiz del sistema esta en:"<<endl;
+			x3.print_vector();
+			cout<<"¿Desea resolver otro sistema?"<<endl<<"1. Si      2. No"<<endl;
+			cin>>p;
+			switch(p){
+				case 1:{
+					break;
+				}
+				case 2:{
+					t=0;
+					break;
+				}
+				default:{
+					cout<<"Elige entre las opciones 1 o 2"<<endl;
+					break;
+				}
+			}
 			
 			break;
 		}
@@ -207,6 +416,7 @@ int main(){
 		
 		
 	}
+}
 	
 	
 	return 0;
