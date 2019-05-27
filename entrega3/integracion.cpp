099834f9600bc3980029fe89bf6e84f7d0f8bc64
ThereminB <barucisaac@gmail.com>
2019-05-26 20:50:55 -0500

    Complemento 3/8

--- entrega3/integracion.cpp
+++ entrega3/integracion.cpp
@@ -1,21 +1,8 @@
 #include "../libraries.h"
 #include "../Vector.h"
 
-float simpson_tercio(float h, Vector fxv, int size){                                      //Vector auxiliar que contendra los valores multiplicados
-    double res, sum = 0;
-    float a = fxv.vector[0];
-    float b = fxv.vector[fxv.get_size()-1];
-    for (int i = 1; i < size - 1; i += 2){
-        sum = sum+(4*fxv.vector[i]);
-    }
-    for (int i = 2; i < size - 2; i += 2){
-        sum = sum+(2*fxv.vector[i]);
-    }
-    res = (h/3)*(a+b+sum);
-    return res;
-}
-
-float simpson_octavo(float h, Vector fxv, int size){
+void simpson_octavo(float h, Vector fxv){
+    int size = fxv.get_size();  
     double res, sum = 0;
     float a = fxv.vector[0];
     float b = fxv.vector[fxv.get_size()-1];
@@ -29,8 +16,43 @@ float simpson_octavo(float h, Vector fxv, int size){
     }
  
     res = 3*h/8*(a+b+sum);
-    return res;   //Se regresa y solo para hacer pruebas
+    cout << "Complementando con regla de 3/8: "<<res<<endl;   //Se regresa y solo para hacer pruebas
 }
+void simpson_tercio(float h, Vector fxv){    
+    int size = fxv.get_size();                                  //Vector auxiliar que contendra los valores multiplicados
+    double res, sum = 0;
+    float a = fxv.vector[0];
+    float b = fxv.vector[fxv.get_size()-1];
+    if((fxv.get_size() - 1 )%2 == 0){
+        for (int i = 1; i < size - 1; i += 2){
+        sum = sum+(4*fxv.vector[i]);
+        }
+        for (int i = 2; i < size - 2; i += 2){
+            sum = sum+(2*fxv.vector[i]);
+        }
+        res = (h/3)*(a+b+sum);
+        cout << "Resultado de la integral con regla de 1/3: "<<res <<endl;
+    }else{
+        for(int i = 0; i < size - 4; i++){
+            for (int j = 1; j < size - 1; j += 2){
+            sum = sum+(4*fxv.vector[j]);
+            }
+            for (int j = 2; j < size - 2; j += 2){
+                sum = sum+(2*fxv.vector[j]);
+            }
+            res = (h/3)*(a+b+sum);    
+        }
+        simpson_octavo(h, fxv.vector[fxv.get_size() - 4]);
+        
+    }
+
+    
+}
+
+
+
+
+
 
 
 
